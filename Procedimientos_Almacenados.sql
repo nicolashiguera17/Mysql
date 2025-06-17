c37c1da120c4dcc91f8bb200a7207d42bc652dfd
nicolashiguera17 <189983438+nicolashiguera17@users.noreply.github.com>
2025-06-17 16:05:51 -0500

    feat: :sparkles: ejercicio 2

--- Procedimientos_Almacenados.sql
+++ Procedimientos_Almacenados.sql
@@ -1,3 +1,4 @@
+-- Active: 1749574161226@@127.0.0.1@3307
 --1.Crea un procedimiento que inserte una nueva pizza en la tabla `pizza` junto con sus ingredientes en `pizza_ingrediente`. --
 
 DELIMITER $$
@@ -60,3 +61,43 @@ BEGIN
 END $$
 DELIMITER ;
 
+-- 2. Procedimiento que reciba `p_pizza_id` y `p_nuevo_precio` y actualice el precio.--
+
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS ps_actualizar_precio_pizza $$
+
+CREATE PROCEDURE ps_actualizar_precio_pizza(
+    IN p_pizza_id INT,
+    IN p_presentacion_id INT,
+    IN p_nuevo_precio DECIMAL(10, 2)
+)
+BEGIN
+
+    IF NOT EXISTS (SELECT 1 FROM producto WHERE id = p_pizza_id) THEN
+        SIGNAL SQLSTATE '40002' SET MESSAGE_TEXT = 'La pizza no existe.';
+    END IF;
+
+    IF NOT EXISTS (SELECT 1 FROM presentacion WHERE id = p_presentacion_id) THEN
+        SIGNAL SQLSTATE '40002' SET MESSAGE_TEXT = 'La presentación no existe.';
+    END IF;
+
+    IF p_nuevo_precio <= 0 THEN
+        SIGNAL SQLSTATE '40001' SET MESSAGE_TEXT = 'El precio debe ser mayor a 0.';
+    END IF;
+
+   
+    UPDATE producto_presentacion
+    SET precio = p_nuevo_precio
+    WHERE producto_id = p_pizza_id AND presentacion_id = p_presentacion_id;
+
+    
+    SELECT pro.nombre AS Producto, pre.precio AS Precio
+    FROM producto pro
+    JOIN producto_presentacion pre ON pre.producto_id = pro.id
+    WHERE pro.id = p_pizza_id AND pre.presentacion_id = p_presentacion_id;
+END$$
+
+DELIMITER ;
+
+CALL ps_actualizar_precio_pizza(1, 3, 14000);
